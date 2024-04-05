bb4ed48225faa462ccca96139150e429d4d75ee4
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-04-05 11:07:35 -0300

    update 2.0

--- bank-in-terminal/src/BankAccount.java
+++ bank-in-terminal/src/BankAccount.java
@@ -1,14 +1,43 @@
 import java.util.Scanner;
 
 public class BankAccount {
-    Scanner scanner = new Scanner(System.in);
-
-    double funds = 25;
+    Scanner sc = new Scanner(System.in);
+    private String agency;
+    private String clientName;
+    private int number;
+    double funds = 237.48;
     String newFunds;
+
+    public BankAccount() {
+        validationFunds();
+    }
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
     public double deposit() {
         System.out.println("Qual o valor que o senhor(a) deseja depositar?");
-        double deposit = scanner.nextDouble();
+        double deposit = sc.nextDouble();
         funds += deposit;
         validationFunds();
         return funds;
@@ -16,7 +45,7 @@ public class BankAccount {
 
     public double withdraw() {
         System.out.println("Qual valor o senhor(a) deseja sacar?");
-        double withdraw = scanner.nextDouble();
+        double withdraw = sc.nextDouble();
         funds -= withdraw;
         validationFunds();
         return funds;
@@ -26,7 +55,8 @@ public class BankAccount {
         newFunds = (funds < 0) ? "Sua conta foi bloqueada, por favor entre em contato conosco." : Double.toString(funds);
     }
 
-    public void printFunds() {
-        System.out.println(newFunds);
+
+    public void showData() {
+        System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + number + " e seu saldo " + newFunds + " já está disponível para saque.");
     }
 }
